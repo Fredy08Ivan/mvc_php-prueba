4078a6e97430bda8ad72b58dcecb9e8d17939e57
Fredy08ivan <lopezruizfredyivan@hotmail.com>
2025-03-27 03:07:16 -0600

    Sistemas de prestamos v0.1

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVERURL="http://localhost/Prestamos_pruebas/";
+
+    const COMPANY = "SISTEMAS PRESTAMOS";
+
+    const MONEDA = "$";
+
+    date_default_timezone_set("America/Monterrey");
